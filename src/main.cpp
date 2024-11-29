23ee6f0f113b230729294459f37aad1f40cb3bd0
fernandezclaudia <cfernandezg@bcds.net>
2024-11-29 13:16:36 +0100

    port/passwd validation

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,44 @@
 #include "IRCServer.hpp"
 
-int main(int argc, char* argv[]) {
-	if (argc != 3) {
-		std::cerr << "Uso: ./ircserv <puerto> <contraseña>" << std::endl;
-		return 1;
-	}
-	// to do validar puerto y la contraseña no hace nada
-	int puerto = std::atoi(argv[1]);
-	std::string password = argv[2];
-	
-	Servidor servidor(puerto, password);
-	if (!servidor.iniciar_servidor()) {
-		std::cerr << "Error al iniciar el servidor." << std::endl;
-		return 1;
-	}
-	servidor.ejecutar();
-	return 0;
+bool	areArgsValid(std::string& port, std::string& passwd)
+{
+	if (!(std::atoi(port.c_str()) >= 1024 && std::atoi(port.c_str()) <= 65535))
+		return (false);
+
+	std::vector<std::string> split_passwd = Utils::splitBySpaces(passwd);
+	if (split_passwd.size() > 1 || std::strlen(passwd.c_str()) > 15)
+		return (false);
+
+	return (true);
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3) {
+        std::cerr << "Usage: ./ircserv <port> <passwd>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::string port = argv[1];
+    std::string password = argv[2];
+
+    if (!areArgsValid(port, password)) {
+        std::cerr << "Error: Invalid arguments." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    try {
+        Servidor servidor(std::atoi(port.c_str()), password);
+        if (!servidor.iniciar_servidor()) {
+            std::cerr << "Error initializing server..." << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        servidor.ejecutar();
+    } catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
